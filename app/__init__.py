6f5344c58b142d19cb35cb2c995a5507d1f4831e
OyugoObonyo <nyikal1999@gmail.com>
2022-01-12 18:02:31 +0300

    Include moments extension in the application

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 from flask_login import LoginManager
+from flask_moment import Moment
 
 
 app = Flask(__name__)
@@ -12,6 +13,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db, render_as_batch=True)
 login = LoginManager(app)
 login.login_view = 'login'
+moment = Moment(app)
 
 
 from app import views
