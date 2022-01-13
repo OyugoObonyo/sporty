3f0311c2197b1140eaa14c52cd6602226eedc6af
OyugoObonyo <nyikal1999@gmail.com>
2022-01-13 16:14:18 +0300

    Register blueprints to the application

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,41 @@ from flask_login import LoginManager
 from flask_moment import Moment
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db, render_as_batch=True)
-login = LoginManager(app)
-login.login_view = 'login'
-moment = Moment(app)
+db = SQLAlchemy()
+migrate = Migrate(db, render_as_batch=True)
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = 'Please log in to view this'
+moment = Moment()
 
 
-from app import views
+def create_app(config_class=Config):
+    """
+    Factory function that creates an instance of a flask application
+    configured and configures it
+    """
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db, render_as_batch=True)
+    login.init_app(app)
+    moment.init_app(app)
+
+    # Register created blueprints to the app
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from app.products import bp as products_bp
+    app.register_blueprint(products_bp)
+
+    return app
+
+
+from app import auth, errors, models
