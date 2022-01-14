582553a979ddd46fabd92cf04048f4b058831ec1
OyugoObonyo <nyikal1999@gmail.com>
2022-01-14 10:11:11 +0300

    Register errors blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -32,6 +32,10 @@ def create_app(config_class=Config):
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp)
 
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
