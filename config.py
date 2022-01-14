c813cb9df1b75fcf0a67e36c5f54fdce5c5c61cb
OyugoObonyo <nyikal1999@gmail.com>
2022-01-14 17:20:37 +0300

    FIX:[ sqlalchemy.exc.NoSuchModuleError]

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ class Config(object):
     A class that stores the apps configuration variables
     """
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    # Modify db env name to cofer with sqlalchemy's new dialect
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1) or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PRODUCT_IMAGES_DIR = "static/images/products"
