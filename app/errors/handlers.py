c31bad2d805ed34a339ce9324dff76f22feae9ee
OyugoObonyo <nyikal1999@gmail.com>
2022-01-14 10:12:24 +0300

    Create handlers for 404 and 500 error codes

---
+++ app/errors/handlers.py
@@ -0,0 +1,21 @@
+from app import db
+from flask import render_template
+from app.errors import bp
+
+
+@bp.app_errorhandler(404)
+def not_found_error(error):
+    """
+    Function renders 404.html page back to user
+    """
+    return render_template('errors/404.html'), 400
+
+
+@bp.app_errorhandler(500)
+def internal_error(error):
+    """
+    Function renders 404.html page back to user
+    """
+    # Ensure failed db session doesn't interfere with db access
+    db.session.rollback()
+    return render_template('errors/500.html'), 500
